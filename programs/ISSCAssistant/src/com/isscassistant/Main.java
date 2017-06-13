ee58ccf37df34b016c57e0d7e4b28d1d41f68b4d
Promatheus <erdei.mate88@gmail.com>
2017-06-13 23:29:29 +0200

    Add ISSCAssistant project

---
+++ programs/ISSCAssistant/src/com/isscassistant/Main.java
@@ -0,0 +1,29 @@
+package com.isscassistant;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			GridPane mainPane = FXMLLoader.load(getClass().getResource("ISSCAssistant.fxml"));
+
+			Scene scene = new Scene(mainPane);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
